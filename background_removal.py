2d2e4bb7d4b2932e487d0d7cec13e6dd47487f03
Arpita Kumari <appu.kumari25@gmail.com>
2022-01-06 13:00:18 +0530

    final code and bg images

--- background_removal.py
+++ background_removal.py
@@ -14,13 +14,36 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 segmentor = SelfiSegmentation(1)
+fpsReader = cvzone.FPS()
 
+listImg = os.listdir("Bg Images")
+imgList = []
+for imgPath in listImg:
+    img = cv2.imread(f'Bg Images/{imgPath}')
+    imgList.append(img)
+    
+indexImg = 0
 
+ 
 while True:
     success, img = cap.read()
-    imgOut = segmentor.removeBG(img, (255, 0, 255))
-    cv2.imshow("Image", img)
-    cv2.imshow("Image Out", imgOut)
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+    imgOut = segmentor.removeBG(img, imgList[indexImg], threshold= 0.85)
+    
+    imgStacked = cvzone.stackImages([img, imgOut], 2, 1)
+    _, imgStacked = fpsReader.update(imgStacked, color = (0, 0, 255))
+    
+    cv2.imshow("Image", imgStacked)
+    key = cv2.waitKey(1)
+    if key == ord('a'):
+        if indexImg==0:
+            indexImg = len(imgList)-1
+        else:
+            indexImg -= 1
+    elif key == ord('d'):
+        if indexImg==len(imgList)-1:
+            indexImg = 0
+        else:
+            indexImg += 1
+    elif key == ord('q'):
         break
 cv2.destroyAllWindows()
